2308a98ed642737ec29bce94f606b3e5f84861c0
bluzi <bluzi@student.42heilbronn.de>
2024-01-31 14:24:33 +0100

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bluzi <bluzi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 15:20:59 by bluzi             #+#    #+#             */
+/*   Updated: 2023/02/09 15:31:16 by bluzi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int		ft_find_nl(const char *s);
+int		ft_strealloc(char **str, size_t size);
+
+char	*make_line(char **remainder, int nl_pos, int rem_len);
+char	*get_next_line(int fd);
+
+#endif
